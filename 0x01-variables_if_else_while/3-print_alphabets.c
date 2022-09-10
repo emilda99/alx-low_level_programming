fd9770103f83475a1071ec4a4c4a8d7c10de4ab2
emilda99 <mukwenhaemilda99@gmail.com>
2022-09-09 20:21:27 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>                                                                                                                                                                      
+#include <stdio.h>                                                                                                                                                                       
+/**                                                                                                                                                                                      
+ * main - enter function to decide number sign                                                                                                                                           
+ * Return: return 0                                                                                                                                                                      
+ */                                                                                                                                                                                      
+int main(void)                                                                                                                                                                           
+{                                                                                                                                                                                        
+char ch;                                                                                                                                                                                 
+for (ch = 'A'; ch <= 'Z'; ch++)                                                                                                                                                          
+putchar(ch);                                                                                                                                                                             
+for (ch = 'a'; ch <= 'z'; ch++)                                                                                                                                                          
+putchar(ch);                                                                                                                                                                             
+return (0);                                                                                                                                                                              
+}
