62572e2333ba7c875d35140ad59eed721d913f74
emilda99 <mukwenhaemilda99@gmail.com>
2022-09-10 23:37:43 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *main-A main function to print out a string
- *Return:Return value set to 0
- */
-int main(void)
+*main-A main function to print out a string
+*Return:Return value set to 0
+*/
+int main (void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 
-  return(0);
+return(0);
 }
