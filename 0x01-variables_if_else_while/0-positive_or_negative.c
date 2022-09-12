417af45beb38b01f4e0519692c7c2113e148e592
emilda99 <mukwenhaemilda99@gmail.com>
2022-09-12 12:04:06 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,27 @@
 #include <stdlib.h>
-#include <time.h>                                                                         #include <stdio.h>                                                                        /**                                                                                
-* main - enter function to decide number sign                                             * Return: return 0                                                                        */
-int main(void)                                                                            {                                                                                         int n;                                                                                    srand(time(0));
-n = rand() - RAND_MAX / 2;                                                                if (n > 0)
-{                                                                                         printf("%d is positive\n", n);                                                            }                                                                                         else if (n == 0)                                                                          {                                                                                         printf("%d is zero\n", n);                                                                }
-else                                                                                      {                                                                                         printf("%d is negative\n", n);                                                            }
-return (0);                                                                               }
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Print if the number is positive, zero or negative.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+return (0);
+}
