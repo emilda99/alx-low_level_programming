ba42ec1a68da7ebe9339b49efb2ec99ad27dfe91
emilda99 <mukwenhaemilda99@gmail.com>
2022-09-19 13:33:35 -0700

    98 Battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef main.h
-#define main.h
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -14,4 +11,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* main.h */
+
