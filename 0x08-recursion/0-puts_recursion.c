cc98e8df398ec9d83245ee104af4899c13c63fe5
emilda99 <mukwenhaemilda99@gmail.com>
2022-09-28 13:45:52 -0700

    in my dreams we are together

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
-* _puts_recursion - uses recursion
-* @s: string to recurse through
+* _puts_recursion - Prints a string followed by a new line
+* @s: string
+*
+* Return: On success 1.
+* On error, -1 is returned, and error is set appropriately.
 */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == 0)
 {
-_putchar(*s);
-_puts_recursion(++s);
-}
-else
 _putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
